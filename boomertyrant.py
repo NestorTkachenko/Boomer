9ef0aee597030b2795dcdf5070513c0a36229eeb
Brian <elisionducoeur@gmail.com>
2019-09-23 23:05:07 -0400

    tyrant done???

--- boomertyrant.py
+++ boomertyrant.py
@@ -8,6 +8,7 @@
 
 import random
 import logging
+import math
 
 from messages import Upload, Request
 from util import even_split
@@ -16,8 +17,10 @@ from peer import Peer
 class BoomerTyrant(Peer):
     def post_init(self):
         print "post_init(): %s here!" % self.id
-        self.dummy_state = dict()
-        self.dummy_state["cake"] = "lie"
+        self.peer_ratios = dict()
+        self.gamma = .1
+        self.r = 3
+        self.alpha = .2
     
     def requests(self, peers, history):
         """
@@ -40,9 +43,9 @@ class BoomerTyrant(Peer):
         for p in peers:
             logging.debug("id: %s, available pieces: %s" % (p.id, p.available_pieces))
 
-        logging.debug("And look, I have my entire history available too:")
-        logging.debug("look at the AgentHistory class in history.py for details")
-        logging.debug(str(history))
+        # logging.debug("And look, I have my entire history available too:")
+        # logging.debug("look at the AgentHistory class in history.py for details")
+        # logging.debug(str(history))
 
         requests = []   # We'll put all the things we want here
         # Symmetry breaking is good...
@@ -68,6 +71,7 @@ class BoomerTyrant(Peer):
                 r = Request(self.id, peer.id, piece_id, start_block)
                 requests.append(r)
 
+        logging.debug("\nrequests for {}:\n{}\n".format(self.id, requests))
         return requests
 
     def uploads(self, requests, peers, history):
@@ -82,6 +86,8 @@ class BoomerTyrant(Peer):
         """
 
         round = history.current_round()
+
+
         logging.debug("%s again.  It's round %d." % (
             self.id, round))
         # One could look at other stuff in the history too here.
@@ -89,22 +95,75 @@ class BoomerTyrant(Peer):
         # has a list of Download objects for each Download to this peer in
         # the previous round.
 
+        if round == 0:
+            if not self.peer_ratios:
+                for peer in peers:
+                    self.peer_ratios[peer.id] = dict()
+                    self.peer_ratios[peer.id]['d'] = 1.0
+                    self.peer_ratios[peer.id]['u'] = 1.0
+        else:
+            # update download rate
+            add_downloads = dict()
+            for download in history.downloads[-1]:
+                if download.from_id not in add_downloads:
+                    add_downloads[download.from_id] = 0
+                add_downloads[download.from_id] += download.blocks
+            for from_id, blocks in add_downloads.items():
+                self.peer_ratios[from_id]['d'] = blocks
+
+            # update upload rate
+            for peer_id, peer_dict in self.peer_ratios.items():
+                if peer_id not in add_downloads:
+                    peer_dict['u'] = peer_dict['u'] * (1+self.alpha)
+                # unchoked for last r periods
+                else:
+                    unchoked = True
+                    # check the last r periods
+                    for i in range(min(self.r, round)):
+                        # is peer in last i period
+                        found_peer = False
+                        for download in history.downloads[-1*(i+1)]:
+                            if download.from_id == peer_id:
+                                found_peer = True
+                                break
+                        # didn't find peer in one of last r periods
+                        if not found_peer:
+                            unchoked = False
+                            break
+                    if unchoked:
+                        peer_dict['u'] = peer_dict['u'] * (1-self.alpha)
+
+        logging.debug("\npeer_ratios for {}:\n{}\n".format(self.id, self.peer_ratios))
+
+        chosen = []
+        bws = []
         if len(requests) == 0:
             logging.debug("No one wants my pieces!")
-            chosen = []
-            bws = []
         else:
-            logging.debug("Still here: uploading to a random peer")
+            logging.debug("Still here: uploading to a (hopefully not) random peer")
             # change my internal state for no reason
-            self.dummy_state["cake"] = "pie"
+            # self.dummy_state["cake"] = "pie"
 
-            request = random.choice(requests)
-            chosen = [request.requester_id]
+            bw_left = self.up_bw
+            upload_to = sorted([(peer_id, d['d']/d['u'], d['u']) for peer_id, d in self.peer_ratios.items()], key=lambda p:p[1], reverse=True)
+            for peer_id, ratio, bw in upload_to:
+                if (bw_left - bw) < 0 or 'Seed' in peer_id:
+                    continue
+                chosen.append(peer_id)
+                bws.append(bw)
+                bw_left -= bw
+
+            logging.debug("\npriorities for {}:\n{}".format(self.id, upload_to))
+
+            # request = random.choice(requests)
+            # chosen = [request.requester_id]
             # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
+            # bws = even_split(self.up_bw, len(chosen))
 
         # create actual uploads out of the list of peer ids and bandwidths
         uploads = [Upload(self.id, peer_id, bw)
                    for (peer_id, bw) in zip(chosen, bws)]
+
+        logging.debug("\nuploads for {} with bw {}:\n{}\n{}\n".format(self.id, self.up_bw, chosen, bws))
             
         return uploads
