1212e3c7bd7f0dadb1a64b3dd5e2d5b5dc47049b
NestorTkachenko <40720883+NestorTkachenko@users.noreply.github.com>
2019-09-25 12:01:11 -0400

    Add files via upload

--- boomerpropshare.py
+++ boomerpropshare.py
@@ -23,9 +23,7 @@ class BoomerPropShare(Peer):
         """
         peers: available info about the peers (who has what pieces)
         history: what's happened so far as far as this peer can see
-
         returns: a list of Request() objects
-
         This will be called after update_pieces() with the most recent state.
         """
         needed = lambda i: self.pieces[i] < self.conf.blocks_per_piece
@@ -33,78 +31,129 @@ class BoomerPropShare(Peer):
         np_set = set(needed_pieces)  # sets support fast intersection ops.
 
 
-        logging.debug("%s here: still need pieces %s" % (
-            self.id, needed_pieces))
-
-        logging.debug("%s still here. Here are some peers:" % self.id)
-        for p in peers:
-            logging.debug("id: %s, available pieces: %s" % (p.id, p.available_pieces))
-
-        logging.debug("And look, I have my entire history available too:")
-        logging.debug("look at the AgentHistory class in history.py for details")
-        logging.debug(str(history))
-
-        requests = []   # We'll put all the things we want here
-        # Symmetry breaking is good...
-        random.shuffle(needed_pieces)
+        requests = []
         
-        # Sort peers by id.  This is probably not a useful sort, but other 
-        # sorts might be useful
-        peers.sort(key=lambda p: p.id)
-        # request all available pieces from all peers!
-        # (up to self.max_requests from each)
+        random.shuffle(needed_pieces)
+
+
+        # records the total number of each piece
+        total_piece_count = dict()
+
+        # record which pieces are available from each peer
+        for peer in peers:
+            av_set = set(peer.available_pieces)
+            for piece in av_set:
+                # add piece to dictionary if not already present
+                if piece not in total_piece_count.keys():
+                    total_piece_count[piece] = [1, [peer.id]]
+                else:
+                # add to total piece count, and record that this peer has it
+                    total_piece_count[piece][0] += 1
+                    total_piece_count[piece][1].append(peer.id)
+
+        # requests all available pieces from all peers
         for peer in peers:
             av_set = set(peer.available_pieces)
             isect = av_set.intersection(np_set)
             n = min(self.max_requests, len(isect))
-            # More symmetry breaking -- ask for random pieces.
-            # This would be the place to try fancier piece-requesting strategies
-            # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(isect, n):
-                # aha! The peer has this piece! Request it.
-                # which part of the piece do we need next?
-                # (must get the next-needed blocks in order)
-                start_block = self.pieces[piece_id]
-                r = Request(self.id, peer.id, piece_id, start_block)
-                requests.append(r)
 
+            # request all available pieces if possible
+            if self.max_requests >= len(isect):
+                for piece in isect:
+                    start_block = self.pieces[piece]
+                    r = Request(self.id, peer.id, piece, start_block)
+                    requests.append(r)
+
+            # if available pieces exceed max_requests, sort by rarity
+            else:
+                # make a list of pieces and rarity from dictionary
+                piece_rarity = []
+                for piece in isect:
+                    piece_rarity.append((total_piece_count[piece][0], piece))
+
+                # randomize so peers don't have the same priority for equally rare pieces
+                random.shuffle(piece_rarity)
+                # sort from rarest to most common
+                piece_rarity.sort(key = lambda x: x[0])
+                # get n rarest pieces and then shuffle for symmetry breaking
+                pieces = [x[1] for x in piece_rarity[:n]]
+                random.shuffle(pieces)
+
+                for piece in pieces:
+                    start_block = self.pieces[piece]
+                    r = Request(self.id, peer.id, piece, start_block)
+                    requests.append(r)
         return requests
 
     def uploads(self, requests, peers, history):
-        """
-        requests -- a list of the requests for this peer for this round
-        peers -- available info about all the peers
-        history -- history for all previous rounds
 
-        returns: list of Upload objects.
+        rand_frac = 0.1
+        blocks = 0
+        uploads = []
 
-        In each round, this will be called after requests().
-        """
 
         round = history.current_round()
-        logging.debug("%s again.  It's round %d." % (
-            self.id, round))
-        # One could look at other stuff in the history too here.
-        # For example, history.downloads[round-1] (if round != 0, of course)
-        # has a list of Download objects for each Download to this peer in
-        # the previous round.
+
+
+        if round > 0:
+            # previous rounds histories
+            dl_history1 = history.downloads[round-1]
+            dl_history = dict()
+
+            # add to dict
+            for down in dl_history1:
+                source_id = down.from_id
+                if source_id not in dl_history.keys():
+                    dl_history[source_id] = down.blocks
+                else:
+                    dl_history[source_id] += down.blocks
 
         if len(requests) == 0:
-            logging.debug("No one wants my pieces!")
             chosen = []
             bws = []
+
         else:
-            logging.debug("Still here: uploading to a random peer")
-            # change my internal state for no reason
-            self.dummy_state["cake"] = "pie"
-
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-            # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
-
-        # create actual uploads out of the list of peer ids and bandwidths
-        uploads = [Upload(self.id, peer_id, bw)
-                   for (peer_id, bw) in zip(chosen, bws)]
+            # record request and upload amounts
+            all_requesters = []
+            uploaded = dict()
             
+            # store peers who requested
+            for request in requests:
+                request_id = request.requester_id
+                if request_id not in all_requesters:
+                    all_requesters.append(request_id)
+
+            # match peers with how much they uploaded
+            for requester in all_requesters:
+                if requester in dl_history.keys():
+                    uploaded[requester] = dl_history[requester]
+
+            # fine the number of blocks total
+            for uploader in uploaded.keys():
+                blocks += uploaded[uploader]
+
+            # calculate bandwidth fraction for each peer
+            for uploader in uploaded.keys():
+                frac = (uploaded[uploader] / blocks) * (1 - rand_frac)
+                uploaded[uploader] = frac
+
+            # filter out uploaded peers to search for optimistic unchoking candidate
+            rand_candidates = filter(lambda x: x not in uploaded.keys(), all_requesters)
+
+            uploads = []
+            
+            # add peers to upload list with respective bandwidth allocation
+            for uploader in uploaded.keys():
+                frac = uploaded[uploader]
+                uploads.append(Upload(self.id, uploader, int(self.up_bw * frac)))
+
+            # add randomly selected peers with bandwidth allocation
+            if len(rand_candidates) > 0:
+                if len(uploads) == 0:
+                    random_candidate = random.choice(rand_candidates)
+                    uploads.append (Upload(self.id, random_candidate, self.up_bw))
+                else:
+                    random_candidate = random.choice(rand_candidates)
+                    uploads.append (Upload(self.id, random_candidate, self.up_bw * rand_frac))
+
         return uploads
